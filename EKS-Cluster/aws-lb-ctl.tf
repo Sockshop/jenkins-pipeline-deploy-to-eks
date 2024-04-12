37656768f3cd8a631168634787dfc65f89d5f50c
mycynomart <cynomart@gmail.com>
2024-04-12 18:50:45 +0200

    jenkinsfile add

--- EKS-Cluster/aws-lb-ctl.tf
+++ EKS-Cluster/aws-lb-ctl.tf
@@ -1,55 +1,55 @@
-// preparation to deploy AWS alb controller with terraform
-resource "kubernetes_service_account" "service-account" {
- metadata {
-  name      = "aws-load-balancer-controller"
-  namespace = "kube-system"
-  labels = {
-    "app.kubernetes.io/name"      = "aws-load-balancer-controller"
-    "app.kubernetes.io/component" = "controller"
-    }
-    annotations = {
-      "eks.amazonaws.com/role-arn"               = aws_iam_role.lb_role_trust.arn
-      "eks.amazonaws.com/sts-regional-endpoints" = "true"
-    }
-  }
-}
+# // preparation to deploy AWS alb controller with terraform
+# resource "kubernetes_service_account" "service-account" {
+#  metadata {
+#   name      = "aws-load-balancer-controller"
+#   namespace = "kube-system"
+#   labels = {
+#     "app.kubernetes.io/name"      = "aws-load-balancer-controller"
+#     "app.kubernetes.io/component" = "controller"
+#     }
+#     annotations = {
+#       "eks.amazonaws.com/role-arn"               = aws_iam_role.lb_role_trust.arn
+#       "eks.amazonaws.com/sts-regional-endpoints" = "true"
+#     }
+#   }
+# }
 
-resource "helm_release" "alb-controller" {
-  name       = "aws-load-balancer-controller"
-  repository = "https://aws.github.io/eks-charts"
-  chart      = "aws-load-balancer-controller"
-  namespace  = "kube-system"
-  depends_on = [
-    kubernetes_service_account.service-account
-  ]
+# resource "helm_release" "alb-controller" {
+#   name       = "aws-load-balancer-controller"
+#   repository = "https://aws.github.io/eks-charts"
+#   chart      = "aws-load-balancer-controller"
+#   namespace  = "kube-system"
+#   depends_on = [
+#     kubernetes_service_account.service-account
+#   ]
 
-  set {
-    name  = "region"
-    value = var.region
-  }
+#   set {
+#     name  = "region"
+#     value = var.region
+#   }
 
-  set {
-    name  = "vpcId"
-    value = module.vpc.vpc_id
-  }
+#   set {
+#     name  = "vpcId"
+#     value = module.vpc.vpc_id
+#   }
 
-  set {
-    name  = "image.repository"
-    value = "public.ecr.aws/eks/aws-load-balancer-controller:v2.5.4"
-  }
+#   set {
+#     name  = "image.repository"
+#     value = "public.ecr.aws/eks/aws-load-balancer-controller:v2.5.4"
+#   }
 
-  set {
-    name  = "serviceAccount.create"
-    value = "false"
-  }
+#   set {
+#     name  = "serviceAccount.create"
+#     value = "false"
+#   }
 
-  set {
-    name  = "serviceAccount.name"
-    value = "aws-load-balancer-controller"
-  }
+#   set {
+#     name  = "serviceAccount.name"
+#     value = "aws-load-balancer-controller"
+#   }
 
-  set {
-    name  = "clusterName"
-    value = var.cluster_name
-  }
-}
+#   set {
+#     name  = "clusterName"
+#     value = var.cluster_name
+#   }
+# }
