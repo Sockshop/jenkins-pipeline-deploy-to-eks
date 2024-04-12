dccfc243b04cc3649caddf8787a239bae68a5dc1
mycynomart <cynomart@gmail.com>
2024-04-12 22:57:57 +0200

    eks Updated

--- EKS-Cluster/eks-cluster.tf
+++ EKS-Cluster/eks-cluster.tf
@@ -1,13 +1,19 @@
 module "eks" {
     source          = "terraform-aws-modules/eks/aws"
-    version = "~> 19.0"
+    version = "19.15.3"
     cluster_name    = var.cluster_name
-    cluster_version = "1.24"
+    cluster_version = "1.28"
 
     cluster_endpoint_public_access = true
 
     vpc_id                   = module.vpc.vpc_id
     subnet_ids               = module.vpc.private_subnets
+    control_plane_subnet_ids = module.vpc.intra_subnets
+    # no key managed by kms (permission issue)
+    create_kms_key                 = false
+    create_cloudwatch_log_group    = false
+    # disable encyption so we dont need a key
+    cluster_encryption_config      = {}
     
     tags = {
       environment = "dev"
