51fb1dc047266a5ea7055c915dc82b9a7444cd31
mycynomart <cynomart@gmail.com>
2024-04-13 22:19:27 +0200

    eks Updated

--- EKS/cluster.tf
+++ EKS/cluster.tf
@@ -7,6 +7,12 @@ module "eks" {
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
 
+  # no key managed by kms (permission issue)
+  create_kms_key                 = false
+  create_cloudwatch_log_group    = false
+  # disable encyption so we dont need a key
+  cluster_encryption_config      = {}
+
   eks_managed_node_groups = {
     nodes = {
       min_size       = 1
