ca9a0dc0a7f83553d608ccb391d3847735f63d97
mycynomart <cynomart@gmail.com>
2024-04-13 19:22:51 +0200

    eks Updated

---
+++ EKS/provider.tf
@@ -0,0 +1,46 @@
+provider "aws" {
+  region = var.region
+}
+
+provider "kubernetes" {
+  host = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "update-kubeconfig",
+      "--name",
+      var.cluster_name,
+      "--region",
+      var.region,
+      "--kubeconfig",
+      var.kubeconfig_path      
+    ]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "update-kubeconfig",
+      "--name",
+      var.cluster_name,
+      "--region",
+      var.region,
+      "--kubeconfig",
+      var.kubeconfig_path      
+    ]
+    }
+  }
+}
