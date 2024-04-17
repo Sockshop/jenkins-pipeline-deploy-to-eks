ab204b21d54f9e06aa6bacd3495118abe87fbe32
mycynomart <cynomart@gmail.com>
2024-04-17 14:41:54 +0200

    Jenkinsfile Updated

--- EKS/cluster.tf
+++ EKS/cluster.tf
@@ -1,14 +1,19 @@
 #EKS
 module "eks" {
   source                         = "terraform-aws-modules/eks/aws"
-  version = "~> 19.0"
+  version = "~> 20.0"
   cluster_name    = var.cluster_name
-  cluster_version                = "1.28"
+  cluster_version                = "1.29"
   cluster_endpoint_public_access = true
   vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
 
- 
+  
+  
+  # Cluster access entry
+  # To add the current caller identity as an administrator
+  enable_cluster_creator_admin_permissions = true
+
   eks_managed_node_groups = {
     nodes = {
       min_size       = 1
