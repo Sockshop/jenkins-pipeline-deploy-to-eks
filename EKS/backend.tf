f3c6ed139433e7e7cd32edc7b61a34b8d4df936f
mycynomart <cynomart@gmail.com>
2024-04-27 22:29:33 +0200

    jenkins modified

--- EKS/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "sockshop-project-backend"
-    region = "eu-west-3"
-    key    = "sockshop-eks-cluster/terraform.tfstate"
-    //dynamodb_table = "app-table"
-  }
-}
-
